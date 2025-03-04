da246f604493f9c56e650c8944ddcdfeffa19457
jomski <bakapakwanto@gmail.com>
2025-03-04 14:14:38 +0800

    initial repo

---
+++ vanta.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    VANTA.BIRDS({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x0,
+        color1: 0xc03737,
+        color2: 0xffffff,
+        speedLimit: 10.00,
+        quantity: 4.00
+      })
+});
